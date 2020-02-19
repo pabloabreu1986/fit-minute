533ed60396c43e528fb208c368e1e8919cd4febd
pablo.abreu <pablo.abreu1986@gmail.com>
2020-02-19 06:08:24 -0300

    up

--- App.js
+++ App.js
@@ -1,10 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { reset } from 'expo/build/AR';
 
 export default function App() {
+
+  const [state, setstate] = useState(60);
+
+
+
+  const reset = () => {
+    setstate(60);
+  }
+
+  const play = () => {
+    setTimeout(() => {
+      if (state === 0) {
+        return;
+      } else {
+        setstate(state - 1)
+      }
+    }, 1000);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.counter}>
+        <Text>{state}</Text>
+      </View>
+
+      <View style={styles.buttons}>
+
+        <View style={styles.buttonPlay}>
+          <Button 
+            title='Play'
+            onPress={()=>play()}
+          />
+        </View>
+
+        <View style={styles.buttonReset}>
+          <Button 
+            title='Reset'
+            onPress={()=>reset()}
+          />
+        </View>
+
+      </View>
     </View>
   );
 }
@@ -16,4 +57,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  counter: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttons:{
+    flex: 1
+  }
 });
